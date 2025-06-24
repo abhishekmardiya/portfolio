e9d279124b586e178588321cf65d1d86b178e754
abhishekmardiya <mardiyaabhishek@gmail.com>
2025-06-24 18:35:54 +0530

    Add initial project structure with blog, sitemap, and navigation components

---
+++ app/components/mdx.tsx
@@ -0,0 +1,113 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Image from "next/image";
+import Link from "next/link";
+import { createElement } from "react";
+import { highlight } from "sugar-high";
+
+function Table({ data }: { data: { headers: string[]; rows: string[][] } }) {
+  const headers = data.headers.map((header, index) => (
+    <th key={index}>{header}</th>
+  ));
+  const rows = data.rows.map((row, index) => (
+    <tr key={index}>
+      {row.map((cell, cellIndex) => (
+        <td key={cellIndex}>{cell}</td>
+      ))}
+    </tr>
+  ));
+
+  return (
+    <table>
+      <thead>
+        <tr>{headers}</tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+// @ts-ignore
+function CustomLink(props) {
+  const href = props.href;
+
+  if (href.startsWith("/")) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (href.startsWith("#")) {
+    return <a {...props} />;
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+}
+
+// @ts-ignore
+function RoundedImage(props) {
+  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+}
+
+// @ts-ignore
+function Code({ children, ...props }) {
+  const codeHTML = highlight(children);
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+}
+
+function slugify(str: string) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+}
+
+function createHeading(level: number) {
+  const Heading = ({ children }: { children: string }) => {
+    const slug = slugify(children);
+    return createElement(
+      `h${level}`,
+      { id: slug },
+      [
+        createElement("a", {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className: "anchor",
+        }),
+      ],
+      children
+    );
+  };
+
+  Heading.displayName = `Heading${level}`;
+
+  return Heading;
+}
+
+const components = {
+  h1: createHeading(1),
+  h2: createHeading(2),
+  h3: createHeading(3),
+  h4: createHeading(4),
+  h5: createHeading(5),
+  h6: createHeading(6),
+  Image: RoundedImage,
+  a: CustomLink,
+  code: Code,
+  Table,
+};
+
+// @ts-ignore
+export function CustomMDX(props) {
+  return (
+    <MDXRemote
+      {...props}
+      components={{ ...components, ...(props.components || {}) }}
+    />
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,67 @@
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import Footer from "./components/footer";
+import { Navbar } from "./components/nav";
+import "./global.css";
+import { baseUrl } from "./sitemap";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Next.js Portfolio Starter",
+    template: "%s | Next.js Portfolio Starter",
+  },
+  description: "This is my portfolio.",
+  openGraph: {
+    title: "My Portfolio",
+    description: "This is my portfolio.",
+    url: baseUrl,
+    siteName: "My Portfolio",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
+interface ClassNameGenerator {
+  (...classes: (string | undefined | null | false)[]): string;
+}
+
+const cx: ClassNameGenerator = (...classes) =>
+  classes.filter(Boolean).join(" ");
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-black",
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
